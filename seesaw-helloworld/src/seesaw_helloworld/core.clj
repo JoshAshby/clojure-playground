0ea304e379622330e389c18bbdb774fb0e476789
JoshAshby <joshuaashby@joshashby.com>
2013-04-04 10:21:59 -0600

    Add basic new project for getting up to speed with seesaw

---
+++ seesaw-helloworld/src/seesaw_helloworld/core.clj
@@ -0,0 +1,6 @@
+(ns seesaw-helloworld.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ seesaw-helloworld/test/seesaw_helloworld/core_test.clj
@@ -0,0 +1,7 @@
+(ns seesaw-helloworld.core-test
+  (:use clojure.test
+        seesaw-helloworld.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
